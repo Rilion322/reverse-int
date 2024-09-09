311af4bb54481d30f608b2c4dd0797487778e44b
Maksim Dubovik <max200289453@gmail.com>
2024-09-09 17:23:44 +0300

    Add my function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  // Convert the number to a string, split it into an array of characters,
+    // reverse the array, join it back into a string, and convert it back to a number
+    return parseInt(String(number).split('').reverse().join(''), 10);
 }
